bb330561653f8d460f3ef840c33a623d3c0670a5
sanbaar <sandra.baar88@gmail.com>
2023-08-28 19:26:26 +0200

    Transferred the Weather app from Codesandbox to VScode

--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import "./App.css";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in Copenhagen is ${Math.round(response.data.main.temp)}ºC`
-    );
+export default function Search() {
+  const [city, setCity] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [temperature, setTemperature] = useState(null);
+  const [humidity, setHumidity] = useState(null);
+  const [wind, setWind] = useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0efb4fc16a9ed98dc0b3aafd8491d6ad&units=metric`;
+    axios.get(url).then(displayWeather);
+
+    setMessage(`This is the current weather in ${city}`);
   }
 
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=0efb4fc16a9ed98dc0b3aafd8491d6ad&units=metric`;
-  axios.get(url).then(handleResponse);
-  return <h2>Hi</h2>;
+  function changeCity(event) {
+    setCity(event.target.value);
+  }
+
+  function displayWeather(response) {
+    setTemperature(`Temperature: ${Math.round(response.data.main.temp)} ºC`);
+    setHumidity(`Humidity: ${Math.round(response.data.main.humidity)} %`);
+    setWind(`Wind: ${Math.round(response.data.wind.speed)} m/s`);
+  }
+
+  return (
+    <div className="Search">
+      <form onSubmit={handleSubmit}>
+        <input type="search" placeholder="Enter a city" onChange={changeCity} />
+        <input type="submit" value="Search" />
+      </form>
+      <h2>{message}</h2>
+      <ul>
+        <li>{temperature}</li>
+        <li>{humidity}</li>
+        <li>{wind} </li>
+      </ul>
+    </div>
+  );
 }
