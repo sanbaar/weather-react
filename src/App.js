d5b6e8d8c910764fa6426dee2dbce01b11e2c5de
sanbaar <sandra.baar88@gmail.com>
2023-08-29 18:42:49 +0200

    Added github link at the bottom

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <h1>Weather App</h1>
         <Weather />
       </header>
+      <footer className="App-footer">
+        <a
+          href="https://github.com/sanbaar/weather-react.git"
+          target="blank_"
+          className="App-link"
+        >
+          Open-source code{" "}
+        </a>
+        by Sandra B.A.
+      </footer>
     </div>
   );
 }
